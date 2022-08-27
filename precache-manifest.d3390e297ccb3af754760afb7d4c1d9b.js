41c63fe3c54a3af58a926aade4d789d657e14892
Manosrija Nasrin <manosrijanasrin@gmail.com>
2022-08-27 12:19:26 +0530

    Updates

---
+++ precache-manifest.d3390e297ccb3af754760afb7d4c1d9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da4e32323522a0113b197403672c66e",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "2634299cdb9491c19cae",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "41f6be601d215431c3ff",
+    "url": "/box-office/static/js/2.e6cc4b4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.e6cc4b4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2634299cdb9491c19cae",
+    "url": "/box-office/static/js/main.ca8f2898.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
